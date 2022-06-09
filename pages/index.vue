bd566cfbe1523d7488a4c7601cbfd7b340163067
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-10 01:22:06 +0330

    getting the first 20 cards and pass it to the list

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <HomeSearch />
-    <HomeMoviesList />
+    <HomeMoviesList :movies="movies.results" />
+    <HomePagination />
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  async asyncData({ params, $axios }) {
+    if (params.page === undefined) {
+      const movies = await $axios.$get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=f62f750b70a8ef11dad44670cfb6aa57`
+      )
+      return { movies }
+    }
+  },
 }
 </script>
