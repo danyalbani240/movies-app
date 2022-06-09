acb425443980060ebc46ea90efb082cbfa9fcae9
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-10 03:19:47 +0330

    convering genre ids to genre

--- components/Home/MoviesList.vue
+++ components/Home/MoviesList.vue
@@ -1,11 +1,46 @@
 <template>
   <div class="grid grid-cols-3 movies-list">
-    <HomeMovieCard v-for="(item, index) in Array(20)" :key="index" />
+    <HomeMovieCard
+      :title="movie.title"
+      :release_date="movie.release_date"
+      :cover="movie.poster_path"
+      v-for="(movie, index) in convertedMovies"
+      :key="movie.id"
+      :tags="movie.genre"
+    />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: ['movies'],
+  data() {
+    return {
+      genres: [],
+    }
+  },
+  async fetch() {
+    this.genres = await this.$axios
+      .$get(
+        'https://api.themoviedb.org/3/genre/movie/list?api_key=f62f750b70a8ef11dad44670cfb6aa57'
+      )
+      .then((data) => data.genres)
+  },
+  computed: {
+    // converting genre id_s to genre
+    convertedMovies() {
+      let array = []
+      this.movies.forEach((movie) => {
+        let genre = []
+        for (const genreId of movie.genre_ids) {
+          genre.push(this.genres.find((item) => item.id === genreId).name)
+        }
+        array.push({ ...movie, genre })
+      })
+      return array
+    },
+  },
+}
 </script>
 
 <style>
