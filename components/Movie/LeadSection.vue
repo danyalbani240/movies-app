6f5a3a6c1d5c817f65ecb948de8d355582e156bb
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-11 22:10:46 +0330

    lead section dynamic render done

--- components/Movie/LeadSection.vue
+++ components/Movie/LeadSection.vue
@@ -1,51 +1,91 @@
 <template>
   <div class="lead-section flex">
-    <img src="" class="poster rounded-xl" alt="" />
+    <img
+      :src="`https://image.tmdb.org/t/p/original${posterPath}`"
+      class="poster rounded-xl"
+      alt=""
+    />
     <div class="details-container grid">
       <div class="flex items-center justify-between">
         <p class="font-bold text-neutral-black">Budget</p>
-        <p class="text-neutral-black">$170’000’000</p>
+        <p class="text-neutral-black">
+          ${{ budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '’') }}
+        </p>
       </div>
       <div class="flex items-center justify-between">
         <p class="font-bold text-neutral-black">Revenue</p>
-        <p class="text-neutral-black">$772’776’600</p>
+        <p class="text-neutral-black">
+          ${{ revenue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '’') }}
+        </p>
       </div>
       <div class="flex items-center justify-between">
         <p class="font-bold text-neutral-black">Release Date</p>
-        <p class="text-neutral-black">2014-07-30</p>
+        <p class="text-neutral-black">{{ releaseDate }}</p>
       </div>
       <div class="flex items-center justify-between">
         <p class="font-bold text-neutral-black">Runtime</p>
-        <p class="text-neutral-black">2h 1m</p>
+        <p class="text-neutral-black">{{ runtimeToHour }}</p>
       </div>
       <div class="flex items-center justify-between">
         <p class="font-bold text-neutral-black">Score</p>
-        <p class="text-neutral-black">7.9 (23563 votes)</p>
+        <p class="text-neutral-black">
+          {{ score.vote_average }} ({{ score.vote_count }} votes)
+        </p>
       </div>
       <div class="flex items-center justify-between">
         <p class="font-bold text-neutral-black">Genres</p>
         <p class="text-neutral-black text-sm">
-          Action, Science Fiction, Adventure
+          <span v-for="(genre, index) in genres">
+            {{ genre.name }}
+            <span v-if="index !== genres.length - 1">,</span></span
+          >
         </p>
       </div>
       <div class="flex items-center justify-between">
         <p class="font-bold text-neutral-black">IMDB Link</p>
-        <p class="link text-neutral-active underline text-sm cursor-pointer">
+        <a
+          target="_blank"
+          :href="'https://www.imdb.com/title/' + imdb"
+          class="link text-neutral-active underline text-sm cursor-pointer"
+        >
           Link
-        </p>
+        </a>
       </div>
       <div class="flex items-center justify-between">
         <p class="font-bold text-neutral-black">Homepage Link</p>
-        <p class="link text-neutral-active underline text-sm cursor-pointer">
+        <a
+          target="_blank"
+          :href="homePage"
+          class="link text-neutral-active underline text-sm cursor-pointer"
+        >
           Link
-        </p>
+        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: [
+    'posterPath',
+    'budget',
+    'revenue',
+    'releaseDate',
+    'runtime',
+    'score',
+    'genres',
+    'homePage',
+    'imdb',
+  ],
+  computed: {
+    runtimeToHour() {
+      let hours = this.runtime / 60
+      let minutes = Math.round((hours - Math.floor(hours)) * 60)
+      return `${Math.floor(hours)}h ${minutes}m`
+    },
+  },
+}
 </script>
 
 <style>
