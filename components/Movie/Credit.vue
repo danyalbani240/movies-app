1290e0672cbc85131ac02bb058e0262817cbf671
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-11 20:26:42 +0330

    Credit Component Ui Done. Movie Detail Page Ui Done

---
+++ components/Movie/Credit.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="credit">
+      <p class="text-black font-bold text-lg ml-1">Credit:</p>
+      <p class="mt-3 text-sm">
+        Zoe Saldanna , Vin Diesel , Chris Pratt , Bradley Cooper , Lee Pace ,
+        Zoe Saldanna , Vin Diesel , Chris Pratt , Bradley Cooper , Lee Pace and
+        19 more.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.credit {
+  margin-top: 80px;
+  color: #1e1e1e;
+}
+</style>
--- pages/_movie/index.vue
+++ pages/_movie/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <MovieNavigation />
+    <MovieLeadSection />
+    <p class="description">
+      Light years from Earth, 26 years after being abducted, Peter Quill finds
+      himself the prime target of a manhunt after discovering an orb wanted by
+      Ronan the Accuser. Light years from Earth, 26 years after being abducted,
+      Peter Quill finds himself the prime target of a manhunt after discovering
+      an orb wanted by Ronan the Accuser. Light years from Earth, 26 years after
+      being abducted, Peter Quill finds himself the prime target of a manhunt
+      after discovering an orb wanted by Ronan the Accuser.
+    </p>
+    <MovieCredit />
   </div>
 </template>
 
@@ -8,4 +19,9 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.description {
+  color: #1e1e1e;
+  margin-top: 53px;
+}
+</style>
