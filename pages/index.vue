dcecd1a763f188d86bd6d0729e5c9fb2fc12ccac
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-11 21:16:09 +0330

    change the apiKey

--- pages/index.vue
+++ pages/index.vue
@@ -28,9 +28,9 @@ export default {
       page = 1
     }
     if (startDate === undefined && endDate === undefined) {
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=f62f750b70a8ef11dad44670cfb6aa57&page=${page}`
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.apiKey}&page=${page}`
     } else {
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=f62f750b70a8ef11dad44670cfb6aa57&page=${page}&primary_release_date.lte=${endDate}&primary_release_date.gte=${startDate}`
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.apiKey}&page=${page}&primary_release_date.lte=${endDate}&primary_release_date.gte=${startDate}`
     }
 
     const results = await this.$axios.$get(url)
