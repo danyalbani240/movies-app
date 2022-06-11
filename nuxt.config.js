c828743f0ca70a9d8c325c1b1d1d3ed347695c85
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-12 02:02:02 +0330

    routes transitioning

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/style.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~/plugins/date-picker.js'],
@@ -51,5 +51,9 @@ export default {
     scrollBehavior: function (to, from, savedPosition) {
       return { x: 0, y: 0, behavior: 'smooth' }
     },
+  },
+  transition: {
+    name: 'page',
+    mode: 'out-in',
   },
 }
